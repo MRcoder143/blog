0ef2c7f86e945b66b79d56046cb58d50b257dbd3
Moeez Mubashar <moeezmubashar2@gmail.com>
2022-07-28 08:11:15 +0000

    complete comments edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,19 +2,21 @@ class CommentsController < ApplicationController
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(cmt_param)
-		
+
 		redirect_to article_path(@article)
 
 	end
 def edit
 	@article = Article.find(params[:article_id])
 	@comment = Comment.find(params[:id])
-
 end
 def update
-@article = Article.find(params[:article_id])
+
+#@article = Article.find(params[:article_id])
 	@comment = Comment.find(params[:id])
 if @comment.update(cmt_param)
+	@article = Article.find(params[:article_id])
+
 redirect_to @article
 else
 render :edit, status: :unprocessable_entity
@@ -25,6 +27,6 @@ end
 
 	def cmt_param
 		params.require(:comment).permit(:comments,:body,:status)
-		
+
 	end
 end
